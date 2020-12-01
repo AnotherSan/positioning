6ee31bbeca67a72cfbed5241779518c96ec1670b
Ksenia Lebedeva <ksenyalebedi99@mail.ru>
2020-12-01 15:10:05 +0500

    Add files via upload

---
+++ positioning/index.js
@@ -0,0 +1,27 @@
+/*
+    Изменить элементу цвет и ширину можно вот так:
+
+    const element = document.querySelector('.myElement');
+    element.style.color = 'red';
+    element.style.width = '300px';
+*/
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay')
+
+function closeWindow(){
+    modal.style.display = 'none';
+    overlay.style.backgroundColor = 'transparent';
+    overlay.style.display = 'none';
+}
+
+function openWindow(){
+    modal.style.display = 'inline-block';
+    overlay.style.backgroundColor = 'rgba(59, 67, 76, 0.9)';
+    overlay.style.display = 'block';
+}
+
+const closeWindowBtn = document.querySelector('.closeWindowBtn');
+closeWindowBtn.addEventListener('click', closeWindow)
+
+const openWindowBtn = document.querySelector('.openWindowBtn');
+openWindowBtn.addEventListener('click', openWindow)
